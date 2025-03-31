0209e41f35574363939e743727a6ca9d60f830f8
SavioFagundes <akirasfm@gmail.com>
2025-03-31 11:19:23 -0300

    feat: commit finalizando paginacao, implementação de funcao e botao ouvinte em JS

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,9 +1,10 @@
+
 const pokeApi = {}
 
-function convertPokeApiDetailToPokemon(pokeDetail){
+function convertPokeApiDetailToPokemon(pokeDetail) {
     const pokemon = new Pokemon()
+    pokemon.number = pokeDetail.id
     pokemon.name = pokeDetail.name
-    pokemon.number = pokeDetail.order
 
     const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
     const [type] = types
@@ -12,24 +13,23 @@ function convertPokeApiDetailToPokemon(pokeDetail){
     pokemon.type = type
 
     pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+
     return pokemon
 }
 
-pokeApi.getPokemonsDetail = (pokemon) => {
+pokeApi.getPokemonDetail = (pokemon) => {
     return fetch(pokemon.url)
         .then((response) => response.json())
-        .then((convertPokeApiDetailToPokemon))
-
+        .then(convertPokeApiDetailToPokemon)
 }
 
-
 pokeApi.getPokemons = (offset = 0, limit = 5) => {
-    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
     return fetch(url)
         .then((response) => response.json())
         .then((jsonBody) => jsonBody.results)
-        .then((pokemons) => pokemons.map(pokeApi.getPokemonsDetail))
+        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
         .then((detailRequests) => Promise.all(detailRequests))
         .then((pokemonsDetails) => pokemonsDetails)
-        
 }
